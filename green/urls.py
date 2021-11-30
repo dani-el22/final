6d0cb7d8c0241b24a335460ccb58473baa6d12e0
dani-el22 <daniel.escalante@usm.cl>
2021-11-29 22:27:58 -0300

    n

---
+++ green/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.views.generic import TemplateView
+from . import views
+
+app_name = "green"
+
+urlpatterns = [
+    path("",views.home, name="home.html"),
+    path("signup/",views.home, name="home.html")
+]
